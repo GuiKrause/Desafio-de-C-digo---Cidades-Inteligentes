e69cf87d0d7b3a56da555b25f82e6b38db5508dd
GuiKrause <guilhermekrauseramos@gmail.com>
2023-12-21 16:54:26 -0400

    Sistema de login, validação de senha e email feito. Crud funcionando, falta guardar senha com hash

--- script.js
+++ script.js
@@ -1,71 +1,161 @@
-// Cadastrar
+let usuarios = []
 
-const cadastrarUsuario = (dados) => {
-    sistemaGerenciamento.usuarios.push({
-        id: sistemaGerenciamento.usuarios.length + 1,
-        nome: dados.nome,
-        email: dados.email,
-        senha: dados.senha,
-        listaPermissao: dados.listaPermissao,
-        dataCriacao: dados.dataCriacao,
-        dataUltimoLogin: dados.dataUltimoLogin,
-        statusAtivacao: dados.statusAtivacao
-    })
-}
+class Usuario {
 
-// Listar
+    validarEmail = (emailTeste) => {
+        const validacaoEmail = /\S+@\S+\.\S+/;
+        let emailValido = true
+        validacaoEmail.test(emailTeste)
 
-const listarUsuarios = () => {
-    return sistemaGerenciamento.usuarios
-}
-
-// Atualizar
-
-const atualizarUsuario = (nome, novoNome, novoEmail, novaSenha, novalistaPermissao) => {
-    const index = listarUsuarios().findIndex(usuario =>
-        usuario.nome === nome
-    )
-    const usuario = sistemaGerenciamento.usuarios[index]
-    if (novoNome) {
-        usuario.nome = novoNome
-    }
-    if (novoEmail) {
-        usuario.email = novoEmail
-    }
-    if (novaSenha) {
-        usuario.senha = novaSenha
-    }
-    if (novalistaPermissao) {
-        usuario.listaPermissao = novalistaPermissao
-    }
-}
-
-// Deletar
-
-const deletarUsuario = (id) => {
-    const listaUsuariosAtualizada = listarUsuarios().filter((usuarioAtual) => {
-        return usuarioAtual.id !== id;
-    })
-    sistemaGerenciamento.usuarios = listaUsuariosAtualizada
-}
-
-// Sistema de gerenciamento
-
-const sistemaGerenciamento = {
-    permissoes: [cadastrarUsuario, listarUsuarios, atualizarUsuario, deletarUsuario],
-    usuarios: [
-        {
-            id: 1,
-            nome: 'admin',
-            email: 'admin@gmail.com',
-            senha: '12ABcd!@#',
-            listaPermissoes: [],
-            dataCriacao: '20/12/2023',
-            dataUltimoLogin: '20/12/2023',
-            statusAtivacao: []
+        for (let i = 0; i < usuarios.length; i++) {
+            const usuarioEmail = usuarios[i].email
+            if (emailTeste === usuarioEmail) {
+                emailValido = false
+                break
+            }
         }
-    ]
+        return emailValido
+    }
+
+    validarSenha = (sen) => {
+        const senha = sen.split('')
+        const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz'
+        const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        const numeros = '0123456789'
+        const caracteresEspeciais = letrasMinusculas.concat(letrasMaiusculas).concat(numeros)
+        let senhaValida
+        const validacao = []
+
+        const senhaInclui = tipoCaractere => {
+            for (let i = 0; i < senha.length; i++) {
+                const caractere = senha[i];
+                if (tipoCaractere.includes(caractere)) {
+                    validacao.push(true)
+                    break
+                }
+            }
+        }
+
+        const senhaIncluiEspeciais = (tipoCaractere) => {
+            for (let i = 0; i < senha.length; i++) {
+                let caractere = senha[i];
+                if (!tipoCaractere.includes(caractere)) {
+                    validacao.push(true)
+                    break
+                }
+            }
+        }
+
+        senhaInclui(letrasMinusculas)
+        senhaInclui(letrasMaiusculas)
+        senhaInclui(numeros)
+        senhaIncluiEspeciais(caracteresEspeciais)
+
+        if (sen.length > 8 && validacao.length === 4) {
+            senhaValida = true
+        } else {
+            senhaValida = false
+        }
+        return senhaValida
+    }
+
+    constructor(nome, email, senha, permissoes) {
+        this.id = usuarios.length + 1
+        this.nome = nome
+        this.email = email
+        this.senha = senha
+        this.permissoes = permissoes
+        this.dataCriacao = new Date()
+        this.statusLogin = false
+        this.statusAtividade = false
+    }
+
+    // Cadastrar
+
+    cadastrarUsuario = (dados) => {
+        if (this.statusLogin === true) {
+
+            const emailValidado = this.validarEmail(dados[1])
+            
+            if (emailValidado === true) {
+                const senhaValidada = this.validarSenha(dados[2])
+                if (senhaValidada === true) {
+                    let usuario = new Usuario(
+                        dados[0],
+                        dados[1],
+                        dados[2],
+                        dados[3]
+                    )
+                    usuarios.push(usuario)
+                    return usuario
+                } else {
+                    return 'Modelo de senha incorreta'
+                }
+            } else {
+                return 'Email já cadastrado tente outro'
+            }
+        }
+    }
+
+    // Listar
+
+    listarUsuarios = () => {
+        return usuarios
+    }
+
+    // Atualizar
+
+    atualizarUsuario = (email, novoNome, novoEmail, novaSenha, novasPermissoes) => {
+        const index = usuarios.findIndex(usuario =>
+            usuario.email === email
+        )
+        const usuario = usuarios[index]
+        if (novoNome) {
+            usuario.nome = novoNome
+        }
+        if (novoEmail) {
+            usuario.email = novoEmail
+        }
+        if (novaSenha) {
+            usuario.senha = novaSenha
+        }
+        if (novasPermissoes) {
+            usuario.permissoes = novasPermissoes
+        }
+    }
+
+    // Deletar
+
+    deletarUsuario = (id) => {
+        let listaUsuariosAtualizada = usuarios.filter((usuarioAtual) => {
+            return usuarioAtual.id !== id;
+        })
+        usuarios = listaUsuariosAtualizada
+    }
+
+    // Login
+
+    login = (email, senha) => {
+        if (email === this.email && senha === this.senha) {
+            this.dataUltimoLogin = new Date()
+            this.statusLogin = true
+            return 'Login realizado com sucesso'
+        } else {
+            return 'Verifique se o usuário ou a senha estão corretos '
+        }
+    }
+
+    //Login e Logout
+
+    logout = () => {
+        if (this.status === true) {
+            this.statusLogin = false
+        }
+    }
+
 }
+
+
 
 
 // Validar senha
@@ -106,68 +196,76 @@ const validarSenha = (sen) => {
     console.log(validacao)
 
     if (sen.length > 8 && validacao.length === 4) {
-        return 'senha válida'
+        return senha
     } else {
-        return 'senha inválida'
+        return 'senha inválida, tente outra'
+    }
+}
+
+
+//Validar Email
+
+const validarEmail = (email) => {
+    const validacaoEmail = /\S+@\S+\.\S+/;
+    validacaoEmail.test(email)
+
+    for (let i = 0; i < usuarios.length; i++) {
+        const usuarioEmail = usuarios[i].email
+        if (email === usuarioEmail) {
+            return 'email já existente'
+        } else {
+            break
+        }
     }
 }
 
 //Ativar e desativar usuário
 
-const ativarUsuario = (nome) => {
+const alterarStatusAtivacao = (nome, status) => {
     const indice = listarUsuarios().findIndex(usuario =>
         usuario.nome === nome
     )
-    const usuario = sistemaGerenciamento.usuarios[indice]
-    usuario.statusAtivacao = 'Ativo'
-}
-
-const desativarUsuario = (nome) => {
-    const indice = listarUsuarios().findIndex(usuario =>
-        usuario.nome === nome
-    )
-    const usuario = sistemaGerenciamento.usuarios[indice]
-    usuario.statusAtivacao = 'Inativo'
-}
-
-//Login e Logout
-
-const login = (nome, senha) => {
-    const listaUsuarios = sistemaGerenciamento.usuarios
-    const indice = listarUsuarios().findIndex(usuario =>
-        usuario.nome === nome
-    )
-    const nomeUsuario = listaUsuarios[indice].nome
-    const senhaUsuario = listaUsuarios[indice].senha
-
-    if (nomeUsuario === nome && senhaUsuario === senha) {
-        return 'Login realizado com sucesso'
-    } else {
-        return 'Verifique se o usuário ou a senha estão corretos '
+    const usuario = usuarios[indice]
+    if (status === 'Ativar') {
+        usuario.statusAtividade = true
+    } else if (status === 'Desativar') {
+        usuario.statusAtivacao = false
     }
 }
 
-const logout = () => {
 
-}
+// Sistema de gerenciamento
+
+
 
 //_____________
 
-cadastrarUsuario({
-    nome: 'Altenir',
-    email: 'alt@gmail.com',
-    senha: '246',
-    listaPermissao: sistemaGerenciamento.permissoes,
-    dataCriacao: '20/12/2023',
-    dataUltimoLogin: '20/12/2023',
-    statusAtivacao: ''
-})
-cadastrarUsuario({
-    nome: 'Serjão dos Foguetes',
-    email: 'serjão@gmail.com',
-    senha: '111',
-    listaPermissao: [],
-    dataCriacao: '20/12/2023',
-    dataUltimoLogin: '20/12/2023',
-    statusAtivacao: ''
-})
+let gui = new Usuario(
+    'Guilherme',
+    'gui@gmail.com',
+    'admin',
+    [],
+)
+
+// cadastrarUsuario({
+//     nome: 'Guilherme',
+//     email: 'gui@gmail.com',
+//     senha: '111',
+//     permissoes: [],
+// })
+
+// cadastrarUsuario({
+//     nome: 'Serjão dos Foguetes',
+//     email: 'serjão@gmail.com',
+//     senha: '333',
+//     permissoes: [],
+// })
+
+//Criptografia
+
+// const string = 'djfghksd'
+// let newString = []
+// for (let i = 0; i < string.length; i++) {
+//     newString.push(string.charCodeAt(i))
+// }
+// console.log(newString)
